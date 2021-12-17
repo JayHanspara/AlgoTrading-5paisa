82c04c7d2b9db3b95a5de5f9f1e791778d8ada41
JayHanspara <jhanspara@gmail.com>
2021-12-17 10:03:04 +0530

    added comments

--- algo.py
+++ algo.py
@@ -6,8 +6,8 @@ content = output.text
 parse_json = json.loads(content)
 nsedata = parse_json['data']
 print (nsedata)
-for exact in nsedata:
+for exact in nsedata: # function to print NSE data only
     exactdata = exact['s']
     #nselist = re.findall(',exactdata)
     #print (nselist)
-    print (exactdata)
+    print (exactdata) # printing data with BSE and NSE both
